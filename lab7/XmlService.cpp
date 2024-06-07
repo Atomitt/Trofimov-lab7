1c4ab70942b916baae86108f4bdf82963f4934cf
Atomitt <elasd1415@gmai.com>
2024-06-07 23:23:33 +0300

    Fix XmlService

--- lab7/XmlService.cpp
+++ lab7/XmlService.cpp
@@ -15,7 +15,7 @@ Weather XmlService::getWeather(std::string s)
     double lat = node.child("city").child("coord").attribute("lat").as_int(); // 58.5966
     double temperature = node.child("temperature").attribute("value").as_double(); // 5.69
     std::string weather = node.child("weather").attribute("value").as_string(); // дождь
-    double windSpeed = node.child("wing").child("speed").attribute("value").as_double();; // 4.27
+    double windSpeed = node.child("wind").child("speed").attribute("value").as_double();; // 4.27
     int clouds = node.child("clouds").attribute("value").as_int(); // 100
     return Weather(city,lon,lat,temperature,weather,windSpeed,clouds);
 }
