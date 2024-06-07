8f09e53a0f525f972c5848eb4492afb38a1479e9
Atomitt <elasd1415@gmai.com>
2024-06-07 23:21:55 +0300

    Test XmlService

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -1,10 +1,11 @@
 ﻿#include <iostream>
 #include "Weather.h"
-#include "JsonService.h"
 #include "Service.h"
+#include "XmlService.h"
 
 int main()
 {
-	JsonService js;
-	Weather w = js.getWeather("weather.json");
+	XmlService xs;
+	Weather w = xs.getWeather("weather.xml");
+
 }
