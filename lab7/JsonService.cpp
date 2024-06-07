0229bc799984d3ae94cb6612e851c0030a654485
Atomitt <elasd1415@gmai.com>
2024-06-07 22:56:59 +0300

    Fix JsonService

--- lab7/JsonService.cpp
+++ lab7/JsonService.cpp
@@ -14,9 +14,9 @@ Weather JsonService::getWeather(std::string s)
     j = json::parse(fin);
     std::string city = j["name"]; //  Киров
     double lon = j["coord"]["lon"]; // 49.6601
-    double lat = j["coord"]["lan"]; // 58.5966  
+    double lat = j["coord"]["lat"]; // 58.5966  
     double temperature = j["main"]["temp"]; // 5.69
-    std::string weather = j["weather"]["main"]; // дождь
+    std::string weather = j["weather"][0]["main"]; // дождь
     double windSpeed = j["wind"]["speed"]; // 4.27
     int clouds = j["clouds"]["all"]; // 100
     return Weather(city,lon,lat,temperature,weather,windSpeed,clouds);
