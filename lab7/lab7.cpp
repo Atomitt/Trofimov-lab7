8e81dc45e7d568a96cb12f6cc113d4506a93c9c0
Atomitt <elasd1415@gmai.com>
2024-06-07 23:26:58 +0300

    Merge dev feature-xml

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -1,11 +1,14 @@
 ﻿#include <iostream>
 #include "Weather.h"
 #include "Service.h"
+#include "JsonService.h"
 #include "XmlService.h"
 
 int main()
 {
 	XmlService xs;
 	Weather w = xs.getWeather("weather.xml");
+	JsonService js;
+	Weather w = js.getWeather("weather.json");
 
 }
